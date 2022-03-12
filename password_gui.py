3e911ce7602d4d8ac52de665f7148a94b815a01a
kimiekomi <kimiekomi1@gmail.com>
2022-03-12 11:01:25 -0700

    created password_gui and gitignore

---
+++ password_gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+from gui_window import App
+
+debug = True
+trace = True
+
+
+class PasswordApplication(App):
+    def __init__(self, title, width, height):
+        super().__init__(title, width, height)
+
+
+if __name__ == "__main__":
+    app = PasswordApplication(title="Password GUI", width=200, height=100)
+    app.mainloop()
